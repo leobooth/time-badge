69f216f7ed1d30f9c822f3ec106adc123ae77bf0
Leo Booth <leoboothtx@gmail.com>
2025-03-28 03:40:57 -0500

    update extension title to show current time, including AM/PM

--- background.js
+++ background.js
@@ -1,29 +1,40 @@
-function getCurrentTimeString(time12Or24) {
+function getCurrentTimeString(is24Hour, isAMPMVisible) {
     let now = new Date();
     let hours = "";
     let minutes = "";
+    let ampm = "";
+    let currentTimeString = "";
 
-
-    if (time12Or24 === 12) {
-        hours = (now.getHours() % 12).toString();
-    } else {
+    if (is24Hour) {
         hours = now.getHours().toString();
+        ampm = "";
+    } else {
+        hours = (now.getHours() % 12).toString();
+        ampm = now.getHours() < 12 ? "am" : "pm";
     }
 
     hours = hours.padStart(2, '0');
     minutes = now.getMinutes().toString().padStart(2, '0');
 
-    return `${hours}:${minutes}`;
+    if(isAMPMVisible) {
+        currentTimeString = `${hours}:${minutes}${ampm}`;
+    } else {
+        currentTimeString = `${hours}:${minutes}`;
+    }
+
+    return currentTimeString;
 }
 
-// TODO: set time12or24 based on click of badge (toggle between 12 or 24)
+// TODO: toggle is24Hour based on click of extension icon/badge
 
 // update the badge with the current time
 // https://developer.chrome.com/docs/extensions/reference/api/action
 function updateBadge() {
-    let timeString = getCurrentTimeString(12);
-    chrome.action.setBadgeText({ text: timeString });
+    let badgeTime = getCurrentTimeString(false, false);
+    let titleTime = getCurrentTimeString(false, true);
+    chrome.action.setBadgeText({ text: badgeTime });
     chrome.action.setBadgeBackgroundColor({ color: "#000000" });
+    chrome.action.setTitle({title: titleTime})
 }
 
 // Initial update and set interval to refresh every second
