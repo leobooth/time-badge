6a0383ec2e600aed5a027da9af6df06a5ede603c
Leo Booth <leoboothtx@gmail.com>
2025-03-28 03:12:18 -0500

    starting work on 12-hour display

--- background.js
+++ background.js
@@ -1,14 +1,27 @@
-function getCurrentTimeString() {
+function getCurrentTimeString(time12Or24) {
     let now = new Date();
-    let hours = now.getHours().toString().padStart(2, '0');
-    let minutes = now.getMinutes().toString().padStart(2, '0');
+    let hours = "";
+    let minutes = "";
+
+
+    if (time12Or24 === 12) {
+        hours = (now.getHours() % 12).toString();
+    } else {
+        hours = now.getHours().toString();
+    }
+
+    hours = hours.padStart(2, '0');
+    minutes = now.getMinutes().toString().padStart(2, '0');
+
     return `${hours}:${minutes}`;
 }
+
+// TODO: set time12or24 based on click of badge (toggle between 12 or 24)
 
 // update the badge with the current time
 // https://developer.chrome.com/docs/extensions/reference/api/action
 function updateBadge() {
-    let timeString = getCurrentTimeString();
+    let timeString = getCurrentTimeString(12);
     chrome.action.setBadgeText({ text: timeString });
     chrome.action.setBadgeBackgroundColor({ color: "#000000" });
 }
