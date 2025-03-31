bf2869280c30f2a3fd0bfe24ea52fec33d7b40d2
Leo Booth <leoboothtx@gmail.com>
2025-03-31 13:19:26 -0500

    moved all global settings to 'gbl_options' object; fixed options after storage and toggle

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-let gbl_is24HourFormat = false
-let gbl_isAMPMVisible = true;
+let gbl_options = {}
+gbl_options.is24HourFormat = false;
 
-function getCurrentTimeString(is24HourFormat, isAMPMVisible, displayFullLengthTime) {
+function getCurrentTimeString(is24HourFormat, displayFullLengthTime) {
     let now = new Date();
     let hours = "";
     let minutes = "";
@@ -30,9 +30,7 @@ function getCurrentTimeString(is24HourFormat, isAMPMVisible, displayFullLengthTi
         }
 
         minutes = now.getMinutes().toString().padStart(2, '0');
-    }
 
-    if(isAMPMVisible) {
         if (displayFullLengthTime) {
             ampm = hours24 < 12 ? "am" : "pm";
             currentTimeString = `${hours}:${minutes}${ampm}`;
@@ -52,55 +50,65 @@ function getCurrentTimeString(is24HourFormat, isAMPMVisible, displayFullLengthTi
     return currentTimeString;
 }
 
-// get user preference for 12 or 24-hour time format
-async function getTimeFormatFromStorage() {
-    const data = await chrome.storage.sync.get("options");
-    return data.options.is24HourFormat;
-}
-
-// set user preference for 12 or 24-hour time format
-async function setTimeFormatToStorage(is24HourFormat) {
-    const options = {}
-    options.is24HourFormat = is24HourFormat;
-    chrome.storage.sync.set({options});
-}
-
-// Toggle between 12-hour and 24-hour time format when the user clicks the extension icon
-//https://developer.chrome.com/docs/extensions/reference/api/storage
-chrome.action.onClicked.addListener(async () => {
-    let formatFromStorage = await getTimeFormatFromStorage();
-    if (formatFromStorage === undefined) {
-        formatFromStorage = gbl_is24HourFormat;
-    }
-
-    gbl_is24HourFormat = !formatFromStorage;
-    gbl_isAMPMVisible = !gbl_isAMPMVisible;
-    await setTimeFormatToStorage(gbl_is24HourFormat);
-    updateTime();
-});
-
 // update the badge with the current time
 // https://developer.chrome.com/docs/extensions/reference/api/action
-function updateBadge(is24HourFormat, isAMPMVisible) {
-    let badgeTime = getCurrentTimeString(is24HourFormat, isAMPMVisible, false);
+function updateBadge(options) {
+    let badgeTime = getCurrentTimeString(options.is24HourFormat, false);
     chrome.action.setBadgeText({ text: badgeTime });
     chrome.action.setBadgeBackgroundColor({ color: "#000000" });
 }
 
 // update the on-hover title with the current time
-function updateTitle(is24HourFormat, isAMPMVisible) {
-    let titleTime = getCurrentTimeString(is24HourFormat, isAMPMVisible, true);
+function updateTitle(options) {
+    let titleTime = getCurrentTimeString(!options.is24HourFormat, true);
     chrome.action.setTitle({title: titleTime})
 }
 
 // if the badge shows 12-hour time, the title will show 24-hour time
 // and vice versa when the badge is toggled
 function updateTime() {
-    updateBadge(gbl_is24HourFormat, gbl_isAMPMVisible);
-    updateTitle(!gbl_is24HourFormat, !gbl_isAMPMVisible);
+    updateBadge(gbl_options);
+    updateTitle(gbl_options);
 }
 
+// get user preference for 12 or 24-hour time format
+async function getOptionsFromStorage() {
+    const data = await chrome.storage.sync.get("options");
+    return data.options;
+}
+
+// set user preference for 12 or 24-hour time format
+async function setOptionsToStorage(options) {
+    chrome.storage.sync.set({options});
+}
+
+// Toggle between 12-hour and 24-hour time format when the user clicks the extension icon
+//https://developer.chrome.com/docs/extensions/reference/api/storage
+chrome.action.onClicked.addListener(async () => {
+    let options = await getOptionsFromStorage();
+
+    console.log("options from storage:");
+    console.log(options);
+
+    if (options === undefined) {
+        const options = {};
+        options.is24HourFormat = gbl_options.is24HourFormat;
+    }
+
+    gbl_options.is24HourFormat = !options.is24HourFormat;
+
+    console.log("gbl_options after toggle:");
+    console.log(gbl_options);
+
+    await setOptionsToStorage(gbl_options);
+    updateTime();
+});
+
+
 // Perform an initial update and set interval to refresh every second
+console.log("initial gbl_options");
+console.log(gbl_options);
+setOptionsToStorage(gbl_options).then();
 updateTime();
 let badgeIntervalId = setInterval(updateTime, 1000);
 
@@ -118,5 +126,7 @@ chrome.idle.onStateChanged.addListener(function(newSystemState) {
 
 // Ensure time badge updates when Chrome starts
 chrome.runtime.onStartup.addListener(() => {
-   updateTime();
+    console.log("got options after startup");
+    gbl_options = getOptionsFromStorage();
+    updateTime();
 });
